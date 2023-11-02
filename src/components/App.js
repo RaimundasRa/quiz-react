644d3174fee57bca2aac0c8f0f613ad7006a2dd1
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-11-02 11:47:07 +0000

    handling new answers

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,8 @@ const initialState = {
     // 'loading', 'error', 'ready', 'finished'
     status: "loading",
     index: 0,
+    answer: null,
+    points: 0,
 };
 
 function reducer(state, action) {
@@ -30,13 +32,23 @@ function reducer(state, action) {
             };
         case "start":
             return { ...state, status: "active" };
+        case "newAnswer":
+            const question = state.questions.at(state.index);
+            return {
+                ...state,
+                answer: action.payload,
+                points:
+                    action.payload === question.correctOption
+                        ? state.points + question.points
+                        : state.points,
+            };
         default:
             throw new Error("Action unknown");
     }
 }
 
 export default function App() {
-    const [{ questions, status, index }, dispatch] = useReducer(
+    const [{ questions, status, index, answer, points }, dispatch] = useReducer(
         reducer,
         initialState
     );
@@ -64,7 +76,11 @@ export default function App() {
                     />
                 )}
                 {status === "active" && (
-                    <Question question={questions[index]} />
+                    <Question
+                        question={questions[index]}
+                        dispatch={dispatch}
+                        answer={answer}
+                    />
                 )}
             </Main>
         </div>
